e7a72534d5536a8af1dddb7b6b065c0a1f4b385c
Nizar Esmat <nizaresmat2000@gmail.com>
2024-08-24 22:30:39 +0300

    not fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-<HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
